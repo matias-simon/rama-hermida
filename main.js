55584bd5d952aa98e0503063569a3f8921c4787a
matias-simon <matias.simon@hotmail.com>
2023-06-30 21:31:42 -0300

    js refactor

--- main.js
+++ main.js
@@ -6,24 +6,46 @@ document.addEventListener("DOMContentLoaded", function () {
     if (storedLanguage === "en") {
       htmlElement.setAttribute("lang", "en");
       englishToggle.checked = true;
-      setEnglish(); // Call the setEnglish() function to update the text
+      setText("en");
     }
   
     englishToggle.addEventListener("change", function () {
       if (englishToggle.checked) {
         htmlElement.setAttribute("lang", "en");
         localStorage.setItem("language", "en");
-        setEnglish();
+        setText("en");
       } else {
         htmlElement.setAttribute("lang", "es");
         localStorage.setItem("language", "es");
-        setSpanish(); // Call the setSpanish() function to reset the text to Spanish
+        setText("es");
       }
     });
   });
   
-  const setEnglish = () => {
-    // Update text to English
+  const setText = (language) => {
+    const text = {
+      en: {
+        navContact: "Contact",
+        youtubeP: "My work on Youtube!!",
+        spotifyP: "Check out my mixes!!",
+        formh1: "Get in Touch",
+        formName: "Name",
+        phone: "Phone",
+        formBtn: "Send",
+        formMessage: "Message",
+      },
+      es: {
+        navContact: "Contacto",
+        youtubeP: "Mis Trabajos en Youtube!!",
+        spotifyP: "Escucha mis mezclas!!",
+        formh1: "Contactate",
+        formName: "Nombre",
+        phone: "Nro de Contacto",
+        formBtn: "Enviar",
+        formMessage: "Mensaje",
+      },
+    };
+  
     let navContact = document.getElementById("navContact");
     let youtubeP = document.getElementById("youtubeP");
     let spotifyP = document.getElementById("spotifyP");
@@ -33,34 +55,13 @@ document.addEventListener("DOMContentLoaded", function () {
     let formBtn = document.getElementById("form-btn");
     let formMessage = document.getElementById("form-message");
   
-    navContact.innerHTML = "Contact";
-    youtubeP.innerHTML = "My work on Youtube!!";
-    spotifyP.innerHTML = "Check out my mixes!!";
-    formh1.innerHTML = "Get in Touch";
-    formName.innerHTML = "Name";
-    phone.innerHTML = "Phone";
-    formBtn.innerHTML = "Send";
-    formMessage.innerHTML = "Message";
-  };
-  
-  const setSpanish = () => {
-    // Reset text to Spanish
-    let navContact = document.getElementById("navContact");
-    let youtubeP = document.getElementById("youtubeP");
-    let spotifyP = document.getElementById("spotifyP");
-    let formh1 = document.getElementById("form-h1");
-    let formName = document.getElementById("form-name");
-    let phone = document.getElementById("form-phone");
-    let formBtn = document.getElementById("form-btn");
-    let formMessage = document.getElementById("form-message");
-  
-    navContact.innerHTML = "Contacto";
-    youtubeP.innerHTML = "Mis Trabajos en Youtube!!";
-    spotifyP.innerHTML = "Escucha mis mezclas!!";
-    formh1.innerHTML = "Contactate";
-    formName.innerHTML = "Nombre";
-    phone.innerHTML = "Nro de Contacto";
-    formBtn.innerHTML = "Enviar";
-    formMessage.innerHTML = "Mensaje";
+    navContact.innerHTML = text[language].navContact;
+    youtubeP.innerHTML = text[language].youtubeP;
+    spotifyP.innerHTML = text[language].spotifyP;
+    formh1.innerHTML = text[language].formh1;
+    formName.innerHTML = text[language].formName;
+    phone.innerHTML = text[language].phone;
+    formBtn.innerHTML = text[language].formBtn;
+    formMessage.innerHTML = text[language].formMessage;
   };
   
